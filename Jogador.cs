59d3cb30f51db4ac34abb92e206310270ba83ad8
AymeJu <36730973+AymeJu@users.noreply.github.com>
2018-10-16 11:12:42 -0300

    Add files via upload

---
+++ Jogador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ
+{
+    class Jogador
+    {
+        // 1. Atributos da classe
+        #region Atributos
+        private string _nome;
+        private int _pontos;
+        #endregion
+
+
+        
+
+        }
+    }
